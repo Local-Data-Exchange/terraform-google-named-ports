e72a37bd3c6ddf928f7652bfd06812734cae4468
Rakshit Menpara <rakshit@pandio.com>
2022-02-09 17:42:18 +0530

    Upgrade to Terraform 0.13

--- outputs.tf
+++ outputs.tf
@@ -14,25 +14,26 @@
 
 output "instance_group" {
   description = "self_link of instance group to update"
-  value       = "${var.instance_group}"
+  value       = var.instance_group
 }
 
 output "name" {
   description = "name of the port"
-  value       = "${var.name}"
+  value       = var.name
 }
 
 output "port" {
   description = "port number"
-  value       = "${var.port}"
+  value       = var.port
 }
 
 output "fingerprint" {
   description = "fingerprint from the API response"
-  value       = "${lookup(data.external.named_ports.result, "fingerprint")}"
+  value       = data.external.named_ports.result["fingerprint"]
 }
 
 output "id" {
   description = "id from the API response"
-  value       = "${lookup(data.external.named_ports.result, "id")}"
+  value       = data.external.named_ports.result["id"]
 }
+
